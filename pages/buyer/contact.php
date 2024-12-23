1ddf81228e38b851942a868998b88148d77d74e0
Candra Zulkarnain <candrazulkarnain8@gmail.com>
2024-12-23 20:38:47 +0700

    fix contact

--- pages/buyer/contact.php
+++ pages/buyer/contact.php
@@ -132,7 +132,7 @@
                 <div class="col-12" style="height: 500px;">
                     <div class="position-relative h-100">
                         <iframe class="position-relative w-100 h-100"
-                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1977.5742847656845!2d110.77358245267953!3d-7.558774999449463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMzMnMzEuNiJTIDExMMKwNDYnMjcuNCJF!5e0!3m2!1sen!2sid!4v1734841182975!5m2!1sen!2sid"></iframe>
+                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1977.5742847656845!2d110.77358245267953!3d-7.558774999449463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMzMnMzEuNiJTIDExMMKwNDYnMjcuNCJF!5e0!3m2!1sen!2sid!4v1734841182975!5m2!1sen!2sid"
                             frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
                             tabindex="0"></iframe>
                     </div>
